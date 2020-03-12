09ec4b2aafdc70d4426158401fc84312340c267b
Nirav <niravpatelp129@gmail.com>
2020-03-11 22:06:48 -0400

    updated to use lazy loading for speed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import LoginPage from "./pages/LoginPage/LoginPage";
-import DashboardPage from "./pages/DashboardPage/DashboardPage";
-
 import "./App.css";
+
+// Pages
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage/DashboardPage"));
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/dashboard">
-            <DashboardPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Suspense fallback={<div className="Loading">Loading...</div>}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/dashboard">
+              <DashboardPage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -2,8 +2,6 @@ const loginsReducer = (state = [], action) => {
   switch (action.type) {
     case "REGISTER_USER":
       return [...state, action.payload];
-    case "GET_ALL_USERS":
-      return state;
     default:
       return state;
   }
