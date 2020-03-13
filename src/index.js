cc3904814cf314a01fe0b03cc257734bf58d1232
Nirav <niravpatelp129@gmail.com>
2020-03-13 12:00:29 -0400

    faster imports added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducers from "./redux/reducers";
-import { ThemeProvider } from "@material-ui/core";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
+import App from "./App";
+import reducers from "./redux/reducers";
 import theme from "./themes/DefaultTheme";
 
 const store = createStore(reducers);
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
+  <MuiThemeProvider theme={theme}>
     <Provider store={store}>
       <App />
     </Provider>
-  </ThemeProvider>,
+  </MuiThemeProvider>,
 
   document.getElementById("root")
 );
