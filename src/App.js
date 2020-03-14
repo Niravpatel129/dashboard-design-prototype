9a77cff87cff1399b879c4f55d61d647ec5c5576
Nirav <niravpatelp129@gmail.com>
2020-03-14 14:08:02 -0400

    added demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import "./App.css";
-
 import LoginPage from "./pages/LoginPage/LoginPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+
+import "./App.css";
 
 function App() {
   return (
