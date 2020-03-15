e9002310f2d583e3132b335cc0f16860574be0c7
Nirav <niravpatelp129@gmail.com>
2020-03-14 23:06:36 -0400

    removed cached files --rebase

--- src/redux/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import loginsReducer from "./loginReducer";
-import drawerReducer from "./drawerReducer";
-
-export default combineReducers({ loginsReducer, drawerReducer });
